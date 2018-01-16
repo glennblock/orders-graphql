17a23dd9ca41e89f8653ee101a37c7d75b2564cf
Glenn Block <glenn.block@gmail.com>
2018-01-15 23:11:08 -0800

    resolver

--- Samples.Schemas.Orders/Schema/OrdersSchema.cs
+++ Samples.Schemas.Orders/Schema/OrdersSchema.cs
@@ -1,13 +1,15 @@
+using GraphQL;
 using GraphQL.Types;
 
 namespace Samples.Schemas.Orders
 {
     public class OrdersSchema : Schema
     {
-        public OrdersSchema(OrdersQuery query, OrdersMutation mutation)
+        public OrdersSchema(OrdersQuery query, OrdersMutation mutation, IDependencyResolver resolver)
         {
             Query = query;
             Mutation = mutation;
+            DependencyResolver = resolver;
         }
     }
 }
